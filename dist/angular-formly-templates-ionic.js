031b78dde243e5f83c954010712ed823b1094bcd
mhartington <mikehartington@gmail.com>
2015-03-30 21:25:22 -0400

    feature(gulp): move over grunt, here comes gulp

--- dist/angular-formly-templates-ionic.js
+++ dist/angular-formly-templates-ionic.js
@@ -40,36 +40,9 @@ angular.module('formlyIonic', ['formly'], ["formlyConfigProvider", function conf
 
 }]);
 
-angular.module('formlyIonic').run(['$templateCache', function($templateCache) {
-  'use strict';
-
-  $templateCache.put('fields/ion-checkbox.html',
-    "<ion-checkbox ng-model=model[options.key]>{{options.templateOptions.label}}</ion-checkbox>"
-  );
-
-
-  $templateCache.put('fields/ion-input.html',
-    "<label class=\"item item-input\"><input ng-model=model[options.key] placeholder={{options.templateOptions.placeholder}} type={{options.templateOptions.type}}></label>"
-  );
-
-
-  $templateCache.put('fields/ion-radio.html',
-    "<ion-radio ng-repeat=\"item in options.templateOptions.list\" ng-value=item.value ng-model=model[options.key]>{{ item.text }}</ion-radio>"
-  );
-
-
-  $templateCache.put('fields/ion-range.html',
-    "<div class=\"item range\" ng-class=\"'range-' + options.templateOptions.rangeClass\"><span>{{options.templateOptions.label}}</span> <i class=icon ng-if=options.templateOptions.minIcon ng-class=\"  options.templateOptions.minIcon\"></i> <input type=range min={{options.templateOptions.min}} max={{options.templateOptions.max}} step={{options.templateOptions.step}} value={{options.templateOptions.value}} ng-model=model[options.key]> <i class=icon ng-if=options.templateOptions.maxIcon ng-class=options.templateOptions.maxIcon></i></div>"
-  );
-
-
-  $templateCache.put('fields/ion-textarea.html',
-    "<label class=\"item item-input\"><textarea placeholder={{options.templateOptions.placeholder}} ng-model=model[options.key]></textarea><label></label></label>"
-  );
-
-
-  $templateCache.put('fields/ion-toggle.html',
-    "<ion-toggle ng-model=model[options.key] toggle-class=toggle-{{options.templateOptions.toggleClass}}>{{options.templateOptions.label}}</ion-toggle>"
-  );
-
-}]);
+angular.module("formlyIonic").run(["$templateCache", function($templateCache) {$templateCache.put("ion-checkbox.html","<ion-checkbox ng-model=\"model[options.key]\">\n  {{options.templateOptions.label}}\n</ion-checkbox>\n");
+$templateCache.put("ion-input.html","<label class=\"item item-input\">\n  <input ng-model=\"model[options.key]\" placeholder=\"{{options.templateOptions.placeholder}}\" type=\"{{options.templateOptions.type}}\">\n</label>\n");
+$templateCache.put("ion-radio.html","<ion-radio ng-repeat=\"item in options.templateOptions.list\" ng-value=\"item.value\" ng-model=\"model[options.key]\" >  \n{{ item.text }}    \n</ion-radio>\n");
+$templateCache.put("ion-range.html","  <div class=\"item range\"\n       ng-class=\"\'range-\' + options.templateOptions.rangeClass\">\n\n    <span>\n       {{options.templateOptions.label}}\n    </span>\n\n    <i class=\"icon\"\n       ng-if=\"options.templateOptions.minIcon\"\n       ng-class=\"  options.templateOptions.minIcon\"></i>\n\n    <input type=\"range\"\n           min=\"{{options.templateOptions.min}}\"\n           max=\"{{options.templateOptions.max}}\"\n           step=\"{{options.templateOptions.step}}\"\n           value=\"{{options.templateOptions.value}}\"\n           ng-model=\"model[options.key]\">\n\n    <i class=\"icon\"\n        ng-if=\"options.templateOptions.maxIcon\"\n        ng-class=\"options.templateOptions.maxIcon\">\n    </i>\n  </div>\n");
+$templateCache.put("ion-textarea.html","<label class=\"item item-input\">\n  <textarea placeholder=\"{{options.templateOptions.placeholder}}\" ng-model=\"model[options.key]\"></textarea>\n<label>\n");
+$templateCache.put("ion-toggle.html","<ion-toggle ng-model=\"model[options.key]\" toggle-class=\"toggle-{{options.templateOptions.toggleClass}}\">\n  {{options.templateOptions.label}}\n</ion-toggle>\n");}]);
---
+++ gulpfile.js
@@ -0,0 +1,66 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var template = require('gulp-angular-templatecache');
+var rename = require('gulp-rename');
+var ngAnnotate = require('gulp-ng-annotate');
+var uglify = require('gulp-uglify');
+var gulp = require('gulp');
+var git = require('gulp-git');
+var bump = require('gulp-bump');
+var filter = require('gulp-filter');
+var tag_version = require('gulp-tag-version');
+
+//Build Vars
+var ionicTemplates = 'templates-ionic';
+var prebuildDir = '.tmp';
+var finalName = 'angular-formly-templates-ionic';
+
+
+gulp.task('default', ['build']);
+gulp.task('template', function() {
+  //Lets build them templates first
+  return gulp.src('src/fields/*.html')
+    .pipe(template(ionicTemplates + ".js", {
+      module: "formlyIonic"
+    }))
+    .pipe(gulp.dest(prebuildDir));
+});
+
+// Then save the main provider in the same tmp dir
+gulp.task('mkSrc', function() {
+  return gulp.src('./src/modules/*.js')
+    // .pipe(concat('all.js'))
+    .pipe(gulp.dest('./.tmp/'));
+});
+
+gulp.task('build', ['template', 'mkSrc'], function() {
+  return gulp.src('./.tmp/*.js')
+    .pipe(ngAnnotate())
+    .pipe(concat(finalName + ".js"))
+    .pipe(gulp.dest('./dist'))
+    .pipe(uglify())
+    .pipe(rename({
+      extname: '.min.js'
+    }))
+    .pipe(gulp.dest('./dist'));
+});
+
+function inc(importance) {
+    // get all the files to bump version in
+    return gulp.src(['./package.json', './bower.json'])
+        // bump the version number in those files
+        .pipe(bump({type: importance}))
+        // save it back to filesystem
+        .pipe(gulp.dest('./'))
+        // commit the changed version number
+        .pipe(git.commit('bumps package version'))
+
+        // read only one file to get the version number
+        .pipe(filter('package.json'))
+        // **tag it in the repository**
+        .pipe(tag_version());
+}
+
+gulp.task('patch', function() { return inc('patch'); });
+gulp.task('minor', function() { return inc('minor'); });
+gulp.task('major', function() { return inc('major'); });
