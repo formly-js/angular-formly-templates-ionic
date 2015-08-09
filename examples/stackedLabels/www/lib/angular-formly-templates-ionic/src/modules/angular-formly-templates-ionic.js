fcf4141e97b6e648e91ecad9e418df731ee86c38
James Simoes <james@pocostud.io>
2015-08-09 03:09:31 -0400

    stacked input with example p2

---
+++ examples/stackedLabels/www/lib/angular-formly-templates-ionic/src/modules/angular-formly-templates-ionic.js
@@ -0,0 +1,41 @@
+angular.module('formlyIonic', ['formly'], function configFormlyIonic(formlyConfigProvider) {
+  'use strict';
+
+
+  angular.forEach(['checkbox','input','radio', 'range','textarea','toggle','select'], function(fieldName) {
+    formlyConfigProvider.setType({
+      name: fieldName,
+      templateUrl: getFieldTemplateUrl(fieldName),
+
+    });
+  });
+
+  formlyConfigProvider.templateManipulators.preWrapper.push(function ariaDescribedBy(template, options, scope) {
+    if (options.templateOptions && angular.isDefined(options.templateOptions.description) &&
+      options.type !== 'radio' && options.type !== 'checkbox') {
+      var el = angular.element('<a></a>');
+      el.append(template);
+      var modelEls = angular.element(el[0].querySelectorAll('[ng-model]'));
+      if (modelEls) {
+        el.append(
+          '<p id="' + scope.id + '_description"' +
+              'class="help-block"' +
+              'ng-if="options.templateOptions.description">' +
+            '{{options.templateOptions.description}}' +
+          '</p>'
+        );
+        modelEls.attr('aria-describedby', scope.id + '_description');
+        return el.html();
+      } else {
+        return template;
+      }
+    } else {
+      return template;
+    }
+  });
+
+  function getFieldTemplateUrl(name) {
+    return 'fields/ion-' + name + '.html';
+  }
+
+});
