81e2d04e23603e0900bbfca0660d9d5c25e50abc
charl <charles.jacquin@ambient-it.net>
2015-11-16 18:50:57 +0100

    form validation support

--- src/modules/angular-formly-templates-ionic.js
+++ src/modules/angular-formly-templates-ionic.js
@@ -9,6 +9,19 @@ angular.module('formlyIonic', ['formly'], function configFormlyIonic(formlyConfi
         });
     });
 
+    formlyConfigProvider.setWrapper({
+        name: 'validationWrapper',
+        template:
+        '<formly-transclude></formly-transclude>'+
+        '<div class="validation-error" ng-class="{\'is-dirty\':options.formControl.$dirty,\'is-touched\':options.formControl.$touched,\'is-invalid\':options.formControl.$invalid}"  ng-messages="options.formControl.$error" ng-if="::options.templateOptions.validate">'+
+            '<div ng-messages-include="{{::options.templateOptions.validationsCustom}}" ng-if="::options.templateOptions.validationsCustom"></div>'+
+            '<div ng-message="{{::name}}" ng-repeat="(name, message) in ::options.validation.messages">'+
+                '{{message(options.formControl.$viewValue, options.formControl.$modelValue, this)}}'+
+            '</div>'+
+        '</div>',
+        types: ['checkbox', 'input', 'radio', 'textarea', 'select', 'floating-input', 'stacked-input', 'inline-input']
+    });
+
     formlyConfigProvider.templateManipulators.preWrapper.push(function ariaDescribedBy(template, options, scope) {
         if (options.templateOptions && angular.isDefined(options.templateOptions.description) &&
             options.type !== 'radio' && options.type !== 'checkbox') {
